9c8662317f4286d8a56fd0fb032e069a81b7cb9f
mvrck96 <30476641+mvrck96@users.noreply.github.com>
2019-03-12 21:30:18 +0300

    .cpp version added

---
+++ OS4/OS4cpp/OS4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int unique = 0, i, j, sizeArr;
+	cout << "Длина массива: ";
+	cin >> sizeArr;
+	int *arr = new int[sizeArr];
+
+	for (i = 0; i < sizeArr; i++) {
+		cin >> arr[i];
+	}
+	for (i = 0; i != sizeArr; i++) {
+		for (j = i; j != sizeArr; j++ ) {
+			if ((i != j) && (arr[i] == arr[j])) {
+				unique += 1;
+				break;
+			}
+		}
+	}
+	cout <<"Количество уникальных элементов: " << (sizeArr - unique);
+}
