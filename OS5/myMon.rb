34accef8534eaa146135984d09af91adec2fe833
Ivan <30476641+mvrck96@users.noreply.github.com>
2019-04-29 22:16:24 +0300

    Add files via upload

--- OS5/myMon.rb
+++ OS5/myMon.rb
@@ -8,6 +8,9 @@ def initialize
   @women = new_cond
   @counter = 0
   @inBath = :neutral
+  @chmax = 3
+  @chman = 0
+  @chwomen = 0
 end
 
 def ManIn
@@ -28,6 +31,22 @@ def WomenIn
   end
 end
 
+def ManInCh
+  synchronize do
+      @man.wait if @chman >= :chmax
+      @chman += 1
+      @man.signal
+  end
+end
+
+def WomenInCh
+  synchronize do
+      @women.wait if @chwomen >= :chmax
+      @chmwomen += 1
+      @women.signal
+  end
+end
+
 def ManOut
   synchronize do
     @counter -= 1
@@ -45,31 +64,70 @@ def WomenOut
   end
 end
 
+def ManOutCh
+  synchronize do
+    @chman -= 1
+    @man.signal
+  end
+end
+
+def WomenOutCh
+  synchronize do
+    @chwomen -= 1
+    @women.signal
+  end
+end
+
 vanna = BathControl.new
 
 ticker = 2
+inx = ticker - 1
 
-ticker.times do
+for i in (0..inx) do
   Thread.new do
     sleep rand*5
-    puts Thread.current.to_s + ' мужчина пытается войти в ванную комнату'
+    puts Thread.current.to_s + ' мужчина хочет войти в раздевалку'
+    vanna.ManInCh
+    puts Thread.current.to_s + ' мужчина вошел в раздевалку'
+    sleep rand*1
+    puts Thread.current.to_s + ' мужчина вышел из раздевалки'
+    vanna.ManOutCh
+    puts Thread.current.to_s + ' мужчина хочет войти в ванную комнату'
     vanna.ManIn
     puts Thread.current.to_s + ' мужчина вошёл в ванную комнату'
     sleep rand
     puts Thread.current.to_s + ' мужчина вышел из ванной комнаты'
     vanna.ManOut
+    puts Thread.current.to_s + ' мужчина хочет войти в раздевалку'
+    vanna.ManInCh
+    puts Thread.current.to_s + ' мужчина вошел в раздевалку'
+    sleep rand*1
+    puts Thread.current.to_s + ' мужчина вышел из раздевалки'
+    vanna.ManOutCh
   end
-end
 
-ticker.times do
+
+for i in (0..inx) do
   Thread.new do
     sleep rand * 5
-    puts Thread.current.to_s + ' женщина пытается войти в ванную комнату'
+    puts Thread.current.to_s + ' женщина хочет войти в раздевалку'
+    vanna.WomenInCh
+    puts Thread.current.to_s + ' женщина вошлa в раздевалку'
+    sleep rand*1
+    puts Thread.current.to_s + ' женщина вышлa из раздевалки'
+    vanna.WomenOutCh
+    puts Thread.current.to_s + ' женщина хочет войти в ванную комнату'
     vanna.WomenIn
     puts Thread.current.to_s + ' женщина вошла в ванную комнату'
     sleep rand
     puts Thread.current.to_s + ' женщина вышла из ванной комнаты'
     vanna.WomenOut
+    puts Thread.current.to_s + ' женщина хочет войти в раздевалку'
+    vanna.WomenInCh
+    puts Thread.current.to_s + ' женщина вошлa в раздевалку'
+    sleep rand*1
+    puts Thread.current.to_s + ' женщина вышлa из раздевалки'
+    vanna.WomenOutCh
   end
 end
 
--- OS5/mySem.rb
+++ OS5/mySem.rb
@@ -5,8 +5,12 @@ class BathControl
     @man = Semaphore.new(1)
     @women = Semaphore.new(1)
     @bath = Semaphore.new(1)
+    @chr = Semaphore.new(1)
     @inBath = :neutral
     @counter = 0
+    @chmax = 3
+    @chman = 0
+    @chwomen = 0
   end
 
   def ManIn
@@ -25,6 +29,20 @@ class BathControl
     @women.V
   end
 
+  def ManInCh
+    @man.P
+    @chr.P if @chman >= :chmax
+    @chman += 1
+    @man.V
+  end
+
+  def WomenInCh
+    @women.P
+    @chr.P if @chwomen >= :chmax
+    @chwomen += 1
+    @women.V
+  end
+
   def ManOut
     @man.P
     @counter -= 1
@@ -40,34 +58,71 @@ class BathControl
     @bath.V if @counter == 0
     @women.V
   end
+
+  def ManOutCh
+    @man.P
+    @chman -= 1
+    @chr.V if @chman < :chmax
+  end
+
+  def WomenOutCh
+    @women.P
+    @chwomen -= 1
+    @chr.V if @chwomen < :chmax
+  end
+
 end
 
 vanna = BathControl.new
 
-ticker = 3
+ticker = 2
 inx = ticker - 1
 
 for i in (0..inx) do
   Thread.new do
     sleep rand*5
-    puts Thread.current.to_s + ' дядя хочет войти'
-    vanna.ManIn
-    puts Thread.current.to_s + ' дядя вошел'
+    puts Thread.current.to_s + ' мужчина хочет войти в раздевалку'
+    vanna.ManInCh
+    puts Thread.current.to_s + ' мужчина вошел в раздевалку'
     sleep rand
-    puts Thread.current.to_s + ' дядя вышел'
+    puts Thread.current.to_s + ' мужчина вышел из раздевалки'
+    vanna.ManOutCh
+    puts Thread.current.to_s + ' мужчина хочет войти в ванную комнату'
+    vanna.ManIn
+    puts Thread.current.to_s + ' мужчина вошёл в ванную комнату'
+    sleep rand
+    puts Thread.current.to_s + ' мужчина вышел из ванной комнаты'
     vanna.ManOut
+    puts Thread.current.to_s + ' мужчина хочет войти в раздевалку'
+    vanna.ManInCh
+    puts Thread.current.to_s + ' мужчина вошел в раздевалку'
+    sleep rand
+    puts Thread.current.to_s + ' мужчина вышел из раздевалки'
+    vanna.ManOutCh
   end
 end
 
 for i in (0..inx) do
   Thread.new do
     sleep rand * 5
-    puts Thread.current.to_s + 'тётя хочет войти'
-    vanna.WomenIn
-    puts Thread.current.to_s + 'тётя вошла'
+    puts Thread.current.to_s + ' женщина хочет войти в раздевалку'
+    vanna.WomenInCh
+    puts Thread.current.to_s + ' женщина вошлa в раздевалку'
     sleep rand
-    puts Thread.current.to_s + 'тётя вышла'
+    puts Thread.current.to_s + ' женщина вышлa из раздевалки'
+    vanna.WomenOutCh
+    puts Thread.current.to_s + ' женщина хочет войти в ванную комнату'
+    vanna.WomenIn
+    puts Thread.current.to_s + ' женщина вошла в ванную комнату'
+    sleep rand
+    puts Thread.current.to_s + ' женщина вышла из ванной комнаты'
     vanna.WomenOut
+    puts Thread.current.to_s + ' женщина хочет войти в раздевалку'
+    vanna.WomenInCh
+    puts Thread.current.to_s + ' женщина вошлa в раздевалку'
+    sleep rand
+    puts Thread.current.to_s + ' женщина вышлa из раздевалки'
+    vanna.WomenOutCh
   end
 end
 
